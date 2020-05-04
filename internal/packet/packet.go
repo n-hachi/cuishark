6c731f4a6ca36e518483fdddb87a7393a2d27f71
Nozomu Hatta <nozomuhatta@gmail.com>
2020-05-05 01:48:32 +0900

    Add Src, Dst and LayerType methods.

--- internal/packet/packet.go
+++ internal/packet/packet.go
@@ -13,17 +13,43 @@ func NewLayer(gl gopacket.Layer) (l *Layer) {
 }
 
 type Packet struct {
-	l []*Layer
+	l        []*Layer
+	lt       gopacket.LayerType
+	src, dst gopacket.Endpoint
 }
 
 func NewPacket(gp gopacket.Packet) (p *Packet) {
 	p = new(Packet)
 	for _, gl := range gp.Layers() {
 		p.l = append(p.l, NewLayer(gl))
+		p.lt = gl.LayerType()
 	}
+
+	// Get Source and Destination informatinos.
+	// Note, do 'not' check transport layer.
+	if net := gp.NetworkLayer(); net != nil {
+		nf := net.NetworkFlow()
+		p.src, p.dst = nf.Endpoints()
+	} else if link := gp.LinkLayer(); link != nil {
+		lf := link.LinkFlow()
+		p.src, p.dst = lf.Endpoints()
+	}
+
 	return p
 }
 
 func (p *Packet) Size() (s int) {
 	return len(p.l)
+}
+
+func (p *Packet) LayerType() (lt gopacket.LayerType) {
+	return p.lt
+}
+
+func (p *Packet) Src() (src gopacket.Endpoint) {
+	return p.src
+}
+
+func (p *Packet) Dst() (dst gopacket.Endpoint) {
+	return p.dst
 }
--- internal/packet/packet_test.go
+++ internal/packet/packet_test.go
@@ -1,6 +1,7 @@
 package packet
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/gopacket"
@@ -43,5 +44,35 @@ func TestPacket_Size(t *testing.T) {
 		t.Errorf("size should be 3, but actually size is %d\n", s)
 	} else {
 		t.Log("size value is 3 as expected\n")
+	}
+}
+
+func TestPacket_LayerType(t *testing.T) {
+	gp, _ := GetPacket()
+	p := NewPacket(gp)
+	if fmt.Sprintf("%v", p.LayerType()) != "TCP" {
+		t.Errorf("p.LayerType() should be TCP, but actually %v\n", p.LayerType())
+	} else {
+		t.Log("p.LayerType() is TCP as expected\n")
+	}
+
+}
+func TestPacket_Src(t *testing.T) {
+	gp, _ := GetPacket()
+	p := NewPacket(gp)
+	if fmt.Sprintf("%v", p.Src()) != "10.1.1.2" {
+		t.Errorf("p.Src() should be 10.1.1.2, but actually %v\n", p.Src())
+	} else {
+		t.Log("p.Src() is 10.1.1.2 as expected\n")
+	}
+}
+
+func TestPacket_Dst(t *testing.T) {
+	gp, _ := GetPacket()
+	p := NewPacket(gp)
+	if fmt.Sprintf("%v", p.Dst()) != "10.1.1.1" {
+		t.Errorf("p.Dst() should be 10.1.1.1, but actually %v\n", p.Dst())
+	} else {
+		t.Log("p.Dst() is 10.1.1.1 as expected\n")
 	}
 }
