0abc5056287f6831cd85f6c89a9149b94f78eb69
Nozomu Hatta <nozomuhatta@gmail.com>
2020-09-18 06:52:34 +0900

    Add the process of sliding the window
    Added a new process of sliding the window to deal with packets that
    don't fit in the window.

--- internal/frontend/pane/packet_pane.go
+++ internal/frontend/pane/packet_pane.go
@@ -2,6 +2,7 @@ package pane
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/n-hachi/cuishark/internal/utils"
 	gc "github.com/rthornton128/goncurses"
@@ -10,6 +11,7 @@ import (
 type PacketPane struct {
 	w   *gc.Window
 	idx int
+	top int
 }
 
 func NewPacketPane(w *gc.Window, idx int) *PacketPane {
@@ -27,7 +29,17 @@ func (pp *PacketPane) Reflesh(status *utils.Status) (err error) {
 	pp.w.Clear()
 	_, x := pp.MaxYX()
 
-	for i, p := range status.PacketList() {
+	// Move window if needed
+	pp.slideWindow(status)
+
+	// Window line
+	winLine := 0
+
+	//for i, p := range status.PacketList() {
+	for i := pp.top; i < pp.bottom(status); i++ {
+		// Get print packet
+		p := status.PacketList()[i]
+
 		// Check whether current focused pane is myself, and current line.
 		flg := (status.PacketIdx() == i && status.PaneIdx() == pp.idx)
 
@@ -39,7 +51,8 @@ func (pp *PacketPane) Reflesh(status *utils.Status) (err error) {
 		}
 		s := fmt.Sprintf("%5d %s", i+1, p.Oneline())
 		s = utils.CutStringTail(s, x)
-		pp.w.MovePrint(i, 0, s)
+		pp.w.MovePrint(winLine, 0, s)
+		winLine++
 
 		// Set underline off
 		if flg {
@@ -51,4 +64,25 @@ func (pp *PacketPane) Reflesh(status *utils.Status) (err error) {
 
 	pp.w.Refresh()
 	return nil
+}
+
+func (pp *PacketPane) bottom(status *utils.Status) (bottom int) {
+	height, _ := pp.MaxYX()
+	if height > len(status.PacketList()) {
+		height = len(status.PacketList())
+	}
+	return pp.top + height
+}
+
+func (pp *PacketPane) slideWindow(status *utils.Status) {
+	log.Printf("status.PacketIdx = %d, pp.top = %d", status.PacketIdx(), pp.top)
+	if status.PacketIdx() < pp.top {
+		pp.top = status.PacketIdx()
+		log.Printf("slide upward")
+	}
+	if status.PacketIdx() >= pp.bottom(status) {
+		slide := status.PacketIdx() - pp.bottom(status) + 1
+		pp.top = pp.top + slide
+		log.Printf("slide downward")
+	}
 }
